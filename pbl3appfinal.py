51c39a0c674f80705386ac534a06874e905a6448
zilpatel16 <88820573+zilpatel16@users.noreply.github.com>
2022-05-06 02:57:47 +0530

    Update pbl3appfinal.py

--- pbl3appfinal.py
+++ pbl3appfinal.py
@@ -34,8 +34,7 @@ def main():
 
 	st.markdown(html_templ,unsafe_allow_html=True)
 	st.write("A simple proposal for Covid-19 Diagnosis powered by Deep Learning and Streamlit")
-	st.sidebar.image("C:\Users\zilpa\Desktop\pblphoto.png",width=300)
-
+	
 	image_file = st.sidebar.file_uploader("Upload an X-Ray Image (jpg, png or jpeg)",type=['jpg','png','jpeg'])
 
 	if image_file is not None:
