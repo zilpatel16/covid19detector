8757e150a1d6b33f6e1451a578dba05a40cd4424
zilpatel16 <88820573+zilpatel16@users.noreply.github.com>
2022-05-06 03:06:02 +0530

    Update pbl3appfinal.py

--- pbl3appfinal.py
+++ pbl3appfinal.py
@@ -91,7 +91,8 @@ def main():
 				X_Ray = img.reshape(1,200,200,1)
 
 				# Pre-Trained CNN Model Importing
-				model = tf.keras.models.load_model("C:\Users\zilpa\Downloads\vgg_chest.h5")
+				
+				model = tf.keras.models.load_model(r"C:\Users\zilpa\Downloads\vgg_chest.h5")
 
 				# Diagnosis (Prevision=Binary Classification)
 				diagnosis = model.predict_classes(X_Ray)
